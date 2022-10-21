9bd3db4d7e86d3f9b40e5e8bfeb050332b83b584
Igor Montezuma <85948415+IgorMontezuma20@users.noreply.github.com>
2022-10-21 10:36:22 -0300

    adicionando cores ao background com o palette

--- app/src/main/java/com/example/zooapp/view/DetailFragment.java
+++ app/src/main/java/com/example/zooapp/view/DetailFragment.java
@@ -1,5 +1,7 @@
 package com.example.zooapp.view;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,11 +9,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
+import androidx.palette.graphics.Palette;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.zooapp.R;
 import com.example.zooapp.databinding.FragmentDetailBinding;
 import com.example.zooapp.databinding.FragmentListBinding;
@@ -57,8 +63,32 @@ public class DetailFragment extends Fragment {
             binding.animalLifespan.setText(animal.lifeSpan);
 
             Util.loadImage(binding.animalImage, animal.imageUrl, Util.getProgressDrawable(binding.animalImage.getContext()));
-
+            setupBackgroundColor(animal.imageUrl);
         }
 
+    }
+
+    private void setupBackgroundColor(String url) {
+        Glide.with(this )
+                .asBitmap()
+                .load(url)
+                .into(new CustomTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        Palette.from(resource)
+                                .generate(palette -> {
+                                    Palette.Swatch color = palette.getLightMutedSwatch();
+                                    if(color != null) {
+                                        int intColor = palette.getLightMutedSwatch().getRgb();
+                                        binding.animalLinearLayout.setBackgroundColor(intColor);
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                    }
+                });
     }
 }
